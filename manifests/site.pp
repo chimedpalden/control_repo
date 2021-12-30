f03dc87b01ee0a124fbe891026b30bd6e630a750
Chimed Palden <chimedpaldan@gmail.com>
2021-12-30 14:05:08 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,18 @@
 node default {
+  file { 'root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
+  }
 }
 node 'ip-172-31-87-48.ec2.internal' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
